b17d2a90e2c5b98392ed47982da61dd67c17a7e8
Matthew Knudsvig <66237096+matthewknudsvig@users.noreply.github.com>
2020-10-21 06:08:43 -0500

    Create StatusTrackerApp.java

---
+++ src/main/com/knudsvig/centchallenge/StatusTrackerApp.java
@@ -0,0 +1,15 @@
+package com.knudsvig.centchallenge.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//!  The entry point class for the application
+public class StatusTrackerApplication {
+
+	//!  The entry point method for the application
+	public static void main(String[] args) {
+		SpringApplication.run(StatusTrackerApplication.class, args);
+	}
+
+}
